f8411363961b6669c884afdd42a1270918013929
Andre Soares <codigoaberto.info@gmail.com>
2019-03-03 12:01:23 -0300

    New version

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Dependências
 
-Tenha o Python 3 e o Pip instalado então rode o comando abaixo:
-`pip install pytube moviepy`
+Tenha o Python 3 e o Pip instalado então rode o comando abaixo para instalar as dependências:
+`pip install -r requirements.txt`
 
 Apos instalar rode o script com `python youtube_download.py`
--- youtube_download.py
+++ youtube_download.py
@@ -1,6 +1,4 @@
-import time
-import os
-import glob
+import os, time, glob, shutil
 from pytube import YouTube
 import moviepy.editor as mp
 
@@ -11,31 +9,47 @@ def downloadYouTube(videourl, path):
         if not os.path.exists(path):
             os.makedirs(path)
         yt.download(path, filename=str(round(time.time() * 1000)))
-    except:
+    except Exception as e:
         print("Erro ao baixar link: {}".format(videourl))
+        print("[Error]: {}".format(e))
 
 def convertMp3(video):
     try:
         clip = mp.VideoFileClip(video)
         clip.audio.write_audiofile("{}.{}".format(str(round(time.time() * 1000)), "mp3"))
-    except:
+    except Exception as e:
         print("Erro ao converter: {}".format(video))
+        print("[Error]: {}".format(e))
 
+def moveToDirectory():
+    dst = "./songs_{}".format(str(round(time.time())))
+    songs = glob.glob("./*.mp3")
 
+    if(len(songs) == 0):
+         return False
+
+    try:
+        os.makedirs(dst)
+    except:
+        print("Erro ao criar pasta")
+
+    for s in songs:
+        shutil.move(s, dst)
+
+# Pega lista de vídeos e quebra em arrays
 videos = input("Cole os links separado por vírgula: ")
-
 videos = videos.split(",")
 
+path_video = "./videos_{}".format(str(round(time.time())))
 for video in videos:
-    downloadYouTube(video, "./videos")
+    downloadYouTube(video, path_video)
 
-videos = glob.glob("./videos/*.mp4")
+videos = glob.glob(path_video + "/*.mp4")
 
+# percorre vídeos e converte em música
 for video in videos:
     print(video)
     convertMp3(video)
 
-
-
-# downloadYouTube('https://www.youtube.com/watch?v=ikYQSS5ejEw', './videos')
-# convertMp3("./videos/Salve.mp4", "./music")
+# Move músicas para um diretorio gerado
+moveToDirectory()
